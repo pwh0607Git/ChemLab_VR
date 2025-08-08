4a95327eaecee920c3f6f46c7d7aadfdc5f6b541
pwh0607Git <pwhof0607@gmail.com>
2025-08-08 09:39:41 +0900

    PWH v0.0.3 feat : Pour Shader graph 작성

---
+++ Assets/_PWH/3.Script/TestCode/WaterPour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPour : MonoBehaviour
+{
+    private bool isGrab;
+    private bool canPour;
+
+    void Start()
+    {
+        isGrab = false;
+        canPour = false;
+    }
+
+    void Update()
+    {
+        
+    }
+}
